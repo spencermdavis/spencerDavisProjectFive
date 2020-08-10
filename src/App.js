298b8146c268722356856d1b0a31ab1acf5d6094
spencermdavis <spencermdavis@mac.com>
2020-08-10 18:53:06 -0400

    began mvp styling

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import firebase from "./firebase";
-import "./App.css";
+import "./App.scss";
 
 class App extends Component {
   constructor() {
@@ -74,38 +74,62 @@ class App extends Component {
     return (
       <div className="App">
         <header>
-          <h1>magic: the gathering collection manager</h1>
+          <div className="wrapper">
+            <h1>magic: the gathering collection manager</h1>
 
-          <form action="submit">
-            <label htmlFor="newCard">Add a card to your collection</label>
-            <input
-              onChange={this.handleChange}
-              // this is called "binding your input" and is a project 5 requirement
-              value={this.state.userInput}
-              type="text"
-              id="newCard"
-            />
+            <form action="submit">
+              <label htmlFor="newCard">Add a card to your collection</label>
+              <input
+                onChange={this.handleChange}
+                // this is called "binding your input" and is a project 5 requirement
+                value={this.state.userInput}
+                type="text"
+                id="newCard"
+              />
 
-            <button onClick={this.handleClick}>Add Card</button>
-          </form>
+              <button onClick={this.handleClick}>Add Card</button>
+            </form>
+          </div>
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+            <path
+              fill="#264653"
+              fillOpacity="1"
+              d="M0,224L0,96L288,96L288,32L576,32L576,128L864,128L864,224L1152,224L1152,96L1440,96L1440,320L1152,320L1152,320L864,320L864,320L576,320L576,320L288,320L288,320L0,320L0,320Z"
+            ></path>
+          </svg>
         </header>
-        <h2>{this.state.userInput}</h2>
-        <ul>
-          {this.state.cards.map((newCardObj) => {
-            return (
-              <li key={newCardObj.id}>
-                <p>{newCardObj.card}</p>
-                <button
-                  onClick={() => {
-                    this.deleteBook(newCardObj.id);
-                  }}
-                >
-                  Remove Card
-                </button>
-              </li>
-            );
-          })}
-        </ul>
+        <main>
+          <div className="wrapper">
+            <ul>
+              {this.state.cards.map((newCardObj) => {
+                return (
+                  <li key={newCardObj.id}>
+                    <p>{newCardObj.card}</p>
+                    <button
+                      onClick={() => {
+                        this.deleteBook(newCardObj.id);
+                      }}
+                    >
+                      Remove Card
+                    </button>
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+            <path
+              fill="#2a9d8f"
+              fillOpacity="1"
+              d="M0,224L0,96L288,96L288,32L576,32L576,128L864,128L864,224L1152,224L1152,96L1440,96L1440,320L1152,320L1152,320L864,320L864,320L576,320L576,320L288,320L288,320L0,320L0,320Z"
+            ></path>
+          </svg>
+        </main>
+        <footer>
+          <div className="wrapper">
+            <p>copyright spencer davis 2020</p>
+          </div>
+        </footer>
       </div>
     );
   }
