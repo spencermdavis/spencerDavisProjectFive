a03b2b2d83bd80c8a5de20971607148bc48700d6
spencermdavis <spencermdavis@mac.com>
2020-08-12 19:19:02 -0400

    Updates

---
+++ precache-manifest.73fe5be3c3df141d46f02b1a8a2b194e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c61a392119296e5036da4368cb2f218d",
+    "url": "/spencerDavisProjectFive/index.html"
+  },
+  {
+    "revision": "8d15e97bd8a52b559cf2",
+    "url": "/spencerDavisProjectFive/static/css/main.4263e00f.chunk.css"
+  },
+  {
+    "revision": "186c847c641ddaa387a0",
+    "url": "/spencerDavisProjectFive/static/js/2.5ff10cf9.chunk.js"
+  },
+  {
+    "revision": "70d88436874ad6f0b4ff8dc0e5c19c4e",
+    "url": "/spencerDavisProjectFive/static/js/2.5ff10cf9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d15e97bd8a52b559cf2",
+    "url": "/spencerDavisProjectFive/static/js/main.ad8c6faf.chunk.js"
+  },
+  {
+    "revision": "dab8d88db1a43c13d04c",
+    "url": "/spencerDavisProjectFive/static/js/runtime-main.6efc5dc2.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/spencerDavisProjectFive/precache-manifest.3499424dd02f0f12c0789267ba8b9087.js"
+  "/spencerDavisProjectFive/precache-manifest.73fe5be3c3df141d46f02b1a8a2b194e.js"
 );
 
 self.addEventListener('message', (event) => {
