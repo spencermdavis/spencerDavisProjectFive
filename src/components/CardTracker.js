4de338aa9db86da42313ec57f52540fdb48a455a
spencermdavis <spencermdavis@mac.com>
2020-09-22 15:23:56 -0400

    refactored card tracker as functional component

--- src/components/CardTracker.js
+++ src/components/CardTracker.js
@@ -1,30 +1,28 @@
-import React, { Component } from "react";
+import React from "react";
 import DisplayCard from "./DisplayCard";
 
-class CardTracker extends Component {
-  render() {
-    return (
-      <ul>
-        {/* returns api data for react to append to dom */}
-        {this.props.cards.map((newCardObj, index) => {
-          return (
-            <li key={index} className="displayCard" tabIndex="0">
-              <DisplayCard
-                thisCard={newCardObj}
-                cards={this.props.cards}
-                deleteCard={this.props.deleteCard}
-                image={newCardObj.card.image_uris.normal}
-                name={newCardObj.card.name}
-                mana_cost={newCardObj.card.mana_cost}
-                cmc={newCardObj.card.cmc}
-                oracle_text={newCardObj.card.oracle_text}
-              ></DisplayCard>
-            </li>
-          );
-        })}
-      </ul>
-    );
-  }
-}
+const CardTracker = (props) => {
+  return (
+    <ul>
+      {/* returns api data for react to append to dom */}
+      {props.cards.map((newCardObj, index) => {
+        return (
+          <li key={index} className="displayCard" tabIndex="0">
+            <DisplayCard
+              thisCard={newCardObj}
+              cards={props.cards}
+              deleteCard={props.deleteCard}
+              image={newCardObj.card.image_uris.normal}
+              name={newCardObj.card.name}
+              mana_cost={newCardObj.card.mana_cost}
+              cmc={newCardObj.card.cmc}
+              oracle_text={newCardObj.card.oracle_text}
+            ></DisplayCard>
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
 
 export default CardTracker;
